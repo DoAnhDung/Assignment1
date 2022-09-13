1ec94d0c22bc661a96642c274d09cf306b8fec86
DoAnhDung <77825151+DoAnhDung@users.noreply.github.com>
2022-09-14 05:55:57 +0700

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,10 +1,11 @@
 public class Menu {
     public static void showMenu(){
         System.out.println(" Please choose:");
-        System.out.println(" 0.Update product list");
-        System.out.println(" 1.Sort by make.");
-        System.out.println(" 2.Filter by year");
-        System.out.println(" 3.Exit");
+        System.out.println(" 1.Update product list");
+        System.out.println(" 2.Sort by make.");
+        System.out.println(" 3.Filter by year.");
+        System.out.println(" 4.Show list.");
+        System.out.println(" 5.Exit");
     }
 
     public static void showSecondMenu(){
